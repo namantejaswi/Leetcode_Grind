23ee7d165612500754a9b9429eea35ce0819a645
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-01-14 06:54:39 -0500

    Time: 125 ms (70.13%), Space: 15.2 MB (46.96%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -5,23 +5,40 @@ class Solution:
        #Dp O(n^2) solution 
     
     
-        dp=[float(inf) for i in range(len(nums))]
-        dp[0]=0
+        #dp=[float(inf) for i in range(len(nums))]
+        #dp[0]=0
         
         #print(dp)
         #Each elment in dp represents the minimum steps to reach it
         
         
-        for i in range(len(nums)):
+        #for i in range(len(nums)):
             
             
-            for j in range(0,nums[i]+1):
+         #   for j in range(0,nums[i]+1):
             
-                if i+j<len(nums):
+          #      if i+j<len(nums):
                 
-                    dp[i+j] =min(dp[i+j],dp[i]+1)
+           #         dp[i+j] =min(dp[i+j],dp[i]+1)
                 
                 
-        return (dp[-1])
+        #return (dp[-1])
+             
+        #Greedy O(n) 
         
+        count=0
+        curr=0
+        farthest=0
+        
+        
+        for i in range(len(nums)-1):
+            
+            farthest=max(farthest,i+nums[i])
+            
+            
+            if i==curr:
+                curr=farthest
+                count+=1
                 
+        return count
+        
