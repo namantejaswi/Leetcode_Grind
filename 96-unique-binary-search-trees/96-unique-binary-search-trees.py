d2068eb1257401cc6392d2a930e7fd11fc252c85
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-01 15:08:27 -0500

    Time: 28 ms (89.24%), Space: 13.9 MB (92.93%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -4,9 +4,9 @@ class Solution:
         #Catalan Number if you know you know
         
         
-        catalan= [0 for i in range(n+1)]
-        catalan[0]=1
-        catalan[1]=1
+        #catalan= [0 for i in range(n+1)]
+        #catalan[0]=1
+        #catalan[1]=1
         
         #for i in range(2,n+1):
          #   for j in range(1,i+1):
