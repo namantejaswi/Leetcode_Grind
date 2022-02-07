a0fc2ea7c27445ab2bfb6dfa733b081daf301529
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-07 04:44:33 -0500

    Time: 51 ms (38.76%), Space: 14 MB (88.02%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        
+        freq_map={}
+        
+        for i in s:
+            if i not in freq_map:
+                freq_map[i]=1
+                
+            else:   freq_map[i]+=1
+            
+         
+        print(freq_map)
+        for j in t:
+            
+            if j not in freq_map:   return j
+            if j in freq_map:
+                
+                if freq_map[j]==1:
+                    del freq_map[j]
+                    
+                    
+                else:   freq_map[j]-=1
+                    
+                   
+                
+            
+        
+        return freq_map.keys()
+                    
+                    
+                    
