949d6dbd715d51850967ff3e2657ed842d67eb4e
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-06 23:30:00 -0500

    Time: 35 ms (60.45%), Space: 13.9 MB (96.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -3,16 +3,16 @@ class Solution:
         
     
     
-        dp=[[0 for i in range(n)] for j in range(m)]
+        #dp=[[0 for i in range(n)] for j in range(m)]
         #print(dp)
         
        
-        for col in range(0,n):
-            dp[0][col]=1
+        #for col in range(0,n):
+         #   dp[0][col]=1
        
         
-        for row in range(0,m):
-            dp[row][0]=1
+        #for row in range(0,m):
+         #   dp[row][0]=1
             
         #print(dp)
         
@@ -21,14 +21,20 @@ class Solution:
         #xcordinate n is number of columns
         
         
-        for r in range(1,m):
-            for c in range(1,n):
+        #for r in range(1,m):
+          #  for c in range(1,n):
                 
-                dp[r][c]=dp[r-1][c]+dp[r][c-1]
+         #       dp[r][c]=dp[r-1][c]+dp[r][c-1]
                 
-        return dp[-1][-1]
+        #return dp[-1][-1]
         
         
+        # number of ways = row +col C r 
+        # BUT WE ARE A DUMBFUCK AND START AT 1,1 INDEXED AT 0,0
         
+        #SO ROW-1+COL-1 C R0W-1
+        
+        
+        return int((factorial(m+n-2))/(factorial(m-1)*factorial(n-1)))
         
         
