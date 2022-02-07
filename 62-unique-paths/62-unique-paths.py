4d4c388da27db7c87348fbcbec670052b72d9ebd
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-06 23:08:55 -0500

    Time: 68 ms (5.76%), Space: 13.8 MB (96.54%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+    
+    
+        dp=[[0 for i in range(n)] for j in range(m)]
+        print(dp)
+        
+       
+        for col in range(0,n):
+            dp[0][col]=1
+       
+        
+        for row in range(0,m):
+            dp[row][0]=1
+            
+        #print(dp)
+        
+        
+        #ycordinat m is number of rows
+        #xcordinate n is number of columns
+        
+        
+        for r in range(1,m):
+            for c in range(1,n):
+                
+                dp[r][c]=dp[r-1][c]+dp[r][c-1]
+                
+        return dp[-1][-1]
+        
+        
+        
+        
+        
