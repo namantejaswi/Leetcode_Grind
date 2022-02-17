3cb760f7111a1a28a7ad74554c8cea6ee96c387f
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-16 21:15:59 -0500

    Time: 226 ms (23.61%), Space: 16.4 MB (23.37%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        
+    
+        s=list(s)
+        
+        stack=[]
+        
+        to_remove=set()
+        
+        for i in range(len(s)):
+            
+            if s[i]=="(":
+                stack.append(i)
+
+                
+            elif s[i]==")":
+                if len(stack)!=0:
+                    stack.pop()
+                else:   
+                    to_remove.add(i)
+                    
+                    
+        #print(s)
+        for i in range(len(stack)):
+            to_remove.add(stack[i])
+            
+            
+        #print(to_remove)
+        
+        res=""
+        for i in range(len(s)):
+            if i not in to_remove:
+                res+=s[i]
+                
+        return res
+        
