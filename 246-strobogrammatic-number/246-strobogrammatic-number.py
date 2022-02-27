f3c52bda01e74a7b8db52b3ccfcc7cd0ed26195a
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-27 03:38:54 -0500

    Time: 31 ms (17.32%), Space: 13.3 MB (96.41%) - LeetHub

---
+++ 246-strobogrammatic-number/246-strobogrammatic-number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isStrobogrammatic(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        
+        rotated={'0':'0','1':'1','6':'9','8':'8','9':'6'}
+        
+        
+        left=0
+        right=len(num)-1
+        
+
+        while(left<=right):
+            
+            
+            if num[right] not in rotated or num[left] not in rotated or num[right]!=rotated[num[left]]:
+                return False
+            
+            left+=1
+            right-=1
+            
+        return True
+            
