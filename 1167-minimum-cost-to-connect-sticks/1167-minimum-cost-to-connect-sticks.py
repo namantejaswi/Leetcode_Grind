d2262249c2c1d11c1d08bab65e6a28e28d532898
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-02-27 00:04:03 -0500

    Time: 620 ms (94.84%), Space: 14.7 MB (42.69%) - LeetHub

---
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def connectSticks(self, sticks):
+        """
+        :type sticks: List[int]
+        :rtype: int
+        """
+        
+        
+        total=0
+        heapq.heapify(sticks)
+            
+        #print(sticks)
+        
+        if len(sticks)==1:  return 0
+                
+        while(len(sticks)!=1):
+        
+            combined=heapq.heappop(sticks)+heapq.heappop(sticks)
+            #print(combined)
+            
+            total+=combined
+            heapq.heappush(sticks,combined)
+            
+        
+        return total
+        
