db07ff3b7bf705e54b885f28be9958b71502e102
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-05-06 14:31:22 -0400

    Time: 59 ms (10.91%), Space: 13.9 MB (74.15%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        
+        #Key Idea the larger number should be towards the 0 index
+        
+        num = [int(d) for d in str(num)]
+        
+        mx_pos=len(num)-1
+        
+        p1,p2=0,0        #The positions we swap
+        
+        for i in range(len(num)-1,-1,-1):
+            if num[i]>num[mx_pos]:
+                mx_pos=i
+                
+            # if we find a smaller number while going left we swap it with max
+            #since the loop runs from back we eventually swap with the left most smaller no.
+            
+            elif num[i]<num[mx_pos]:    
+                p1=i
+                p2=mx_pos
+        
+        num[p1],num[p2]=num[p2],num[p1]
+        
+        print(num)
+        
+        res=0
+        p=0
+        
+        for i in range(len(num)-1,-1,-1):
+            res+=num[i]*(10**p)
+            p+=1
+            
+        return res
+        
+
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
