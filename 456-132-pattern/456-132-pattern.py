20d768134bee9cb0579dc6b552c7f7e53a7b9595
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-05-07 18:26:50 -0400

    Time: 517 ms (35.70%), Space: 32 MB (95.87%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -2,7 +2,7 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         
         
-        stack=[]    #array with value and minimum
+        stack=[]    #array with value and minimum so far
         
         mi = nums[0]
         
@@ -12,10 +12,13 @@ class Solution:
             while  stack and i >= stack[-1][0]:
                     
                     stack.pop()
-                    
-                    
-            if stack and  i>stack[-1][1]:
-                    
+            
+            #we maintain a stack such that we have the biggest element on top and while storing the biggest element we also have the smallest element which occured previous to the bigger as we append the variable mi as previous min
+            
+            
+            if stack and i<stack[-1][0] and i>stack[-1][1]:
+                
+            
                 return True
                 
             stack.append([i,mi])
