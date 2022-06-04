c6fc9e7da0a95aeb6cf94dabdd3e7c1b6127b64d
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-06-04 01:23:34 -0400

    Time: 824 ms (50.45%), Space: 23.4 MB (84.87%) - LeetHub

---
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxNonOverlapping(self, nums: List[int], target: int) -> int:
+        
+    
+    #subarray's sum must be equalt to yarget
+    
+        s=set()
+        s.add(0)
+        
+        count=0
+        prefix_sum=0
+        
+        for i in range(len(nums)):
+            
+            prefix_sum+=nums[i]
+            if prefix_sum-target in s:
+                count+=1
+                #if we take an array reset the prefix sum and all the intermediate subarray sum stored in s since that can't be used as we need only non overlapping subarrays
+                s=set()
+                prefix_sum=0
+            s.add(prefix_sum)
+                
+        return count
+                
+                
+                
+            
+            
+        
