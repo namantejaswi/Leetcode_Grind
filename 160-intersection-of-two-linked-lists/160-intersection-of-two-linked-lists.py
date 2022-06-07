04b835123e7be7653c855785e2173ed286ebe64a
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-06-06 20:51:41 -0400

    Time: 194 ms (58.58%), Space: 29.5 MB (69.35%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -8,11 +8,23 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
         
-        pA = headA
-        pB = headB
+        ptrA = headA
+        ptrB = headB
 
-        while pA != pB:
-            pA = headB if pA is None else pA.next
-            pB = headA if pB is None else pB.next
-
-        return pA
+        #in order to make sure moth ptrs of the 2 list travel the same distacne
+        #once we reach the end of list b we move the pointer to the starting of the next list
+        #this is same as calculating the length of the 2 lists and moving the head ptr of the larger list by the difference in the list before starting
+        while ptrA != ptrB:
+            
+            if ptrA is None:
+                ptrA = headB
+            
+            else: 
+                ptrA = ptrA.next
+            
+            if ptrB is None:
+                ptrB = headA
+            
+            else:    ptrB=ptrB.next
+                
+        return ptrA
