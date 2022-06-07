f117396fa0181f7a9701c2dbd8c899e87556aeab
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-06-07 12:03:48 -0400

    Time: 139 ms (19.02%), Space: 14.4 MB (66.55%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        
+        
+        cnt=0
+        
+        flag=0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                
+                
+                if board[i][j]=="X":
+                    
+                    #we count at top left if its either 0 or is the first block i.e nothing in previous row or column
+
+                    if((i>0 and board[i-1][j]=="X") or (j>0 and board[i][j-1]=="X")):
+                        
+                        continue
+                    
+                    else: cnt+=1
+        return cnt
+                
+                
