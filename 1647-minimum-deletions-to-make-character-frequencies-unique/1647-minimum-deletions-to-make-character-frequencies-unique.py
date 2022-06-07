5ff74c23196dd88cc54031500f8d95ce7111db0e
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-06-07 10:26:14 -0400

    Time: 587 ms (5.03%), Space: 14.8 MB (54.01%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -3,16 +3,12 @@ class Solution:
         
         
         freq_map={}
-        
-        s=list(s)
-        
         for i in s:
             if i in freq_map:   
                 freq_map[i]+=1
             else:   
                 freq_map[i]=1
-                
-                
+                   
         cnt={}
         
         for k,v in freq_map.items():
@@ -25,23 +21,17 @@ class Solution:
         cnt=dict(sorted(cnt.items()))
         
         #print(cnt)
-        
         res=0
         s2=set()
         for k,v in cnt.items():
             while len(cnt[k])>1:
                 char=cnt[k].pop()
-                
-                f=k
-                
+                f=k  
                 while((f in cnt or f in s2) and f>0 ):
                         f=f-1
                         res+=1
-                
                 if f>0:
                     s2.add(f)
-            
-    
         return res
         
             
