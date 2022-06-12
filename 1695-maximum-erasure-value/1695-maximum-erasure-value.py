121377ad33474a22019f096899c586291fa2c896
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-06-11 22:14:50 -0400

    Time: 2135 ms (30.64%), Space: 27.4 MB (81.40%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        
+        
+        l=0
+        r=0
+        s=set()
+        mx=0
+        summation=0
+        
+        for r in range(len(nums)):
+           # print(summation,"left",l,"right",r)
+            
+            if nums[r] not in s:
+                s.add(nums[r])
+                summation+=nums[r]
+                #print("adding",nums[r])
+            else:
+                summation+=nums[r]
+                while(l<r):
+                    if nums[l]!=nums[r]:
+                        s.remove(nums[l])
+                        summation-=nums[l]
+                        #print("subtracting",nums[l])
+                        l+=1
+                           
+                    elif nums[l]==nums[r]:
+                        summation-=nums[l]
+                       # print("subtracting",nums[l])
+                        l+=1
+                        break                        
+            
+            #print(summation)
+            #mx=max(mx,sum(nums[l:r+1])) #O(n^2) calculating sum every time
+            mx=max(mx,summation)           
+                
+        return mx
+            
