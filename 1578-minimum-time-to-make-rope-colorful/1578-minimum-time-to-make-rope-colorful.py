084b9b0a0e676bef6c78f1f04ec3a0bb18d5bf06
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-03 19:38:13 -0400

    Time: 3010 ms (14.23%), Space: 25 MB (27.04%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,20 +1,23 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
         
-        
-        last=0
-        total=0
+        time,last=0,0
         for i in range(1,len(colors)):
-            
-            if colors[last]!=colors[i]:
+            if colors[i]!=colors[last]:
                 last=i
                 
             else:
-                total+=min(neededTime[i],neededTime[last])
-                if neededTime[i]>neededTime[last]:
-                    last=i            
-
-        return total
+                time+=min(neededTime[i],neededTime[last])
+                
+                
+                #if removing previous is easier we remove previous
+                #and set last to curr
+                if neededTime[i]>neededTime[last]:  last=i  
+                
+                #else we let previous be last and remove curr
         
-                     
-    #sexy sawal
+        return time
+    
+    
+    
+    
