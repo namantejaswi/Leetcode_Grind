9f56937081d09ea055255c1065e7cf78002d179d
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-08 18:03:09 -0400

    Time: 173 ms (5.29%), Space: 15.6 MB (13.06%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -2,23 +2,24 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         
         
-        #dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-1])
-        #base case dp[0]=cost[0],dp[1]=cost[1]
-        
-        #stupid question top floor or target is one index ahead of last elment in arr
-        
         dp=[-1]*(len(cost)+1)
-        def calc(idx):
-            if idx<=1:  #becase we can start from one or zero
-                dp[idx]=0
-                return dp[idx]
-                        
-            elif dp[idx]!=-1: return dp[idx]
         
+        def calc(idx):
+            
+            if dp[idx]!=-1: return dp[idx]   
+            
+            elif idx<=1:  
+                dp[idx]=0
+                #since we can start at zero or one the minimum cost is zero
+                
+                return dp[idx]
+            
+            
             else:
                 
                 dp[idx]=min(calc(idx-1)+cost[idx-1],calc(idx-2)+cost[idx-2])
                 return dp[idx]
-                
-        calc(len(cost))
-        return dp[-1]
+            
+            
+        return calc(len(cost))
+    
