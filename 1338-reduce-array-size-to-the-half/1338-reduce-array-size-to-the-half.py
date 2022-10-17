47b8e7f9065d496b662e5da15337da8d64abebac
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-17 07:19:50 -0400

    Time: 1493 ms (5.98%), Space: 37.2 MB (15.17%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        
+        l2=math.ceil(len(arr)//2)
+        freq=dict(Counter(arr))
+        
+       
+        
+        h=[]
+        for i in freq.values():
+            h.append(-i)
+        
+        heapq.heapify(h)
+        num=0
+        cnt=0
+        while num<l2 and h:
+            
+            num+=-heapq.heappop(h)
+            cnt+=1
+            
+        return cnt
+        
+        
