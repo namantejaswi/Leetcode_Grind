4c6bf9221010e05f6df554f5aa2534a9b26e5456
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-18 19:17:02 -0400

    Time: 101 ms (86.79%), Space: 27.3 MB (11.11%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,41 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        
+        
+        
+        
+        arr_map=defaultdict(int)
+        
+        
+        for i in nums:
+            
+            if i not in arr_map:    arr_map[i]=i
+            else:   arr_map[i]+=i
+                
+        #@lru_cache    
+        dic={}
+        def calc(n):
+            
+            if n==0:    return 0 #nothing to select
+            if n==1:    return arr_map[1]# we can select only 1 element
+            
+            
+            elif n in dic:   
+                return dic[n]
+            
+            else: 
+                dic[n]= max(calc(n-1),calc(n-2)+arr_map[n])
+                return dic[n]
+
+        return calc(max(nums))
+        
+      
+        
+
+
+        
+
+        
+        
+        
+        
