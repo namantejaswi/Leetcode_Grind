7498bd55797034dbc1137f7208383defa7f0d9a9
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-28 20:49:59 -0400

    Time: 1679 ms (9.70%), Space: 15.4 MB (74.99%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -11,7 +11,7 @@ class Solution:
         #lowest sum(nums)/h
         
         
-        lo = max(1, math.ceil(sum(nums)/h))
+        lo = max(1, math.floor(sum(nums)/h))
         
         hi = max(nums)
         
