8b321dfc9db0304a377673d8bb9e4d4b5619fcef
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-28 20:45:20 -0400

    Time: 1726 ms (8.21%), Space: 15.6 MB (24.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minEatingSpeed(self, nums: List[int], h: int) -> int:
+        
+        
+            
+            
+        #Find the appropiate, the min sufficient hourly rate k using binary search
+        
+        
+        #highest we can have is max(nums)
+        #lowest sum(nums)/h
+        
+        
+        #lo = math.floor(sum(nums)/h) 
+        lo=1
+        hi = max(nums)
+        
+        while lo<hi:
+            
+            mid=(lo+hi)//2
+            
+            
+            t=0
+            
+            for i in nums:
+                
+                t+=math.ceil(i/mid)
+                
+            if t<=h: hi=mid
+                
+            else :   lo = mid+1
+                
+        return lo
