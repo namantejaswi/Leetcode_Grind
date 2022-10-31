9a1c0b3c5863fa173f13aed37d5fc4a0a8e225d2
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-10-31 07:42:44 -0400

    Time: 1502 ms (51.59%), Space: 15.3 MB (39.39%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,47 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        
+        #8 Direction Bfs to find the shortest path
+        
+        if grid[0][0]==1:  return -1    #cant start
+        
+        q=deque()
+        visited=set()
+        l=len(grid)
+        
+        steps=1
+        q.append((0,0,1))   #x,y,length of path
+        
+        visited.add((0,0))
+        
+        while q:
+            
+    
+            x,y,steps=q.popleft()
+        
+            
+            if x==l-1 and y==l-1: 
+                return steps
+            
+            if grid[x][y]==1:   continue
+                
+                
+
+            for i,j in((x+1,y),(x-1,y),(x+1,y+1),(x-1,y+1),(x-1,y-1),(x,y-1),(x,y+1),(x+1,y-1)):
+
+                if i>=0 and j>=0 and i<l and j<l and (i,j) not in visited and grid[i][j]==0:
+
+
+                        q.append((i,j,steps+1))
+                        visited.add((i,j))
+
+            steps+=1
+            
+        return -1
+            
+            
+        
+        
+        
+        
