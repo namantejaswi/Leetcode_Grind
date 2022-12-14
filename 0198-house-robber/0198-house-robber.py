8b5a48e30231af415c16108f53487e0b68b3201d
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-12-14 03:53:56 -0500

    Time: 35 ms (88.52%), Space: 13.8 MB (66.57%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums)<2: return max(nums)
+        
+        dp=[-1 for i in range(len(nums))]
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        
+        def rob(index):
+            
+            if index==len(nums):    return 0
+            
+            if dp[index]!=-1:   return dp[index]
+            
+            
+            else:   
+                
+                pick_current= rob(index-2)+nums[index] 
+                leave_current=rob(index-1)
+                
+                dp[index]=max(pick_current,leave_current)
+                return dp[index]
+                                
+        rob(len(nums)-1)
+        
+        return dp[-1]
