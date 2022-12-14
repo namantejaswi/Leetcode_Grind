1b464676806a460c61e6d011f9d1f312e44a835f
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-12-14 05:53:41 -0500

    Time: 34 ms (91.40%), Space: 13.8 MB (72.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,35 +4,54 @@ class Solution:
         #we calculate by taking 2 inputs 1st from 0 to n-1 and then from 1 to n
         
         
-        if len(nums)<=2:return max(nums)
+        #recurrsive memoized solution
+#         if len(nums)<=2:return max(nums)
         
-        def calc(houses):
+#         def calc(houses):
         
-            dp=[-1 for i in range(len(houses))]
-            dp[0]=houses[0]
-            dp[1]=max(houses[0],houses[1])
+#             dp=[-1 for i in range(len(houses))]
+#             dp[0]=houses[0]
+#             dp[1]=max(houses[0],houses[1])
             
-            def count(index):
+#             def count(index):
             
                 
-                if index<0: return 0# will never actually be here 
+#                 if index<0: return 0# will never actually be here 
                 
-                if dp[index]!=-1:   return dp[index]
+#                 if dp[index]!=-1:   return dp[index]
                 
                 
-                else:
+#                 else:
                     
-                    pick= count(index-2)+houses[index]
-                    no_pick=count(index-1)
+#                     pick= count(index-2)+houses[index]
+#                     no_pick=count(index-1)
                     
-                    dp[index]=max(pick,no_pick)
+#                     dp[index]=max(pick,no_pick)
                     
-                    return dp[index]
+#                     return dp[index]
             
-            count(len(houses)-1)
-            return dp[-1]
+#             count(len(houses)-1)
+#             return dp[-1]
                    
         
+#         nums2=nums[1:]
+#         nums.pop()    
+#         return max(calc(nums[:]), calc(nums2[:]))         
+
+
+        if len(nums)<=2:return max(nums)
+    
         nums2=nums[1:]
-        nums.pop()    
-        return max(calc(nums[:]), calc(nums2[:]))         
+        nums.pop()
+        
+        def calc(nums):
+            dp=[-1 for i in range(len(nums))]
+            dp[0]=nums[0]
+            dp[1]=max(nums[0],nums[1])
+
+            for i in range(2,len(nums)):
+                
+                dp[i]=max(dp[i-2]+nums[i],dp[i-1])
+            return dp[-1]
+        
+        return(max(calc(nums),calc(nums2)))
