b56f3cdf2a7f423d9e2b2a69be98803ac239afba
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-03-14 04:06:33 -0400

    Time: 141 ms (38.86%), Space: 14.7 MB (44.34%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -9,15 +9,12 @@ class Solution:
         for i in range(len(s)):
             
             
-            print(s[i])
-            
             if s[i] in curr_set:
                 cnt+=1
-                print(curr_set)
                 curr_set=set(s[i])
                 
             else:       
                 curr_set.add(s[i])
             
             
-        return cnt+1
+        return cnt+1        
