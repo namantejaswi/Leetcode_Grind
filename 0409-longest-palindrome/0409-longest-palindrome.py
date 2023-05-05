7fcd858754499cf4a2ddbb043204d6a7dd24ac30
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-05-05 07:56:24 -0400

    Time: 48 ms (9.16%), Space: 16.4 MB (8.07%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,49 +1,67 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         
-        
+        res=0
         dic = dict(Counter(s))
-        
-        
-        even =dict()
-        odd=dict()
-        
-        
-        for i in dic.items():
+        flag=True
+        for k,v in dic.items():
             
-            if i[1]%2==0:
+            if v%2==0:  res+=v
                 
-                even[i[0]]=i[1]
+            if v%2==1:
                 
-            else:   odd[i[0]]=i[1]
-                
-        
-        #print(even,odd)
-        
-        res = 0
-        
-        if even:    res+=sum(even.values())
-            
-        mx=0
-        
-        max_key=None
-        
-        for key,val in odd.items():
-            
-            if val>mx:
-                mx=val
-                max_key=key
-                
-        res+=mx
-    
-        if max_key:del(odd[max_key])
-            
-            
-        for key,val in odd.items():
-            
-            res+=val-1
-            
+                if flag:    
+                    res+=v
+                    flag=False
+                elif not flag:    res+=v-1
+                    
+                    
         return res
+                    
+        
+        
+#         dic = dict(Counter(s))
+        
+        
+#         even =dict()
+#         odd=dict()
+        
+        
+#         for i in dic.items():
+            
+#             if i[1]%2==0:
+                
+#                 even[i[0]]=i[1]
+                
+#             else:   odd[i[0]]=i[1]
+                
+        
+#         #print(even,odd)
+        
+#         res = 0
+        
+#         if even:    res+=sum(even.values())
+            
+#         mx=0
+        
+#         max_key=None
+        
+#         for key,val in odd.items():
+            
+#             if val>mx:
+#                 mx=val
+#                 max_key=key
+                
+#         res+=mx
+    
+#         if max_key:del(odd[max_key])
+            
+            
+#         for key,val in odd.items():
+            
+#             res+=val-1
+            
+#         return res
             
         
         
