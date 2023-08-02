6360b383b12cc7e83f0625fe559291f3c034ca19
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-08-02 19:59:55 -0400

    Time: 52 ms (72.00%), Space: 16.6 MB (41.50%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,22 +2,22 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
         
+        res = []
         
+        def rec(curr):
+            
+            if len(curr)==len(nums):
+                
+                res.append(curr[:])
+                return
+            
+            
+            for i in nums:
+                if i not in curr:
+                    curr.append(i)
+                    rec(curr)
+                    curr.pop()
+                    
         res=[]
-        
-        
-        def calc(curr_arr,rem_arr):
-            
-            if not rem_arr:
-                
-                res.append(curr_arr)
-                return res
-            
-            for i in range(len(rem_arr)):
-                
-                calc(curr_arr+[rem_arr[i]],rem_arr[:i]+rem_arr[i+1:])
-                
-        calc([],nums)
+        rec([])
         return res
-                
-            
