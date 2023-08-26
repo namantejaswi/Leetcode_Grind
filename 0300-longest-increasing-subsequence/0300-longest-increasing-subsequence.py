54ce8069516129d123aa170d8ba700f4395c5fff
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-08-26 13:45:07 -0700

    Time: 2444 ms (54.26%), Space: 16.7 MB (65.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,17 +3,16 @@ class Solution:
         
         
         dp = [ 1 for i in range(len(nums))] 
-        
-        # dp[index] stores the longest increasing subsequence till the index
-        
-        for end in range(1, len(nums)):     #the subsequence ending at endd
+                
             
-            for start in range(0, end ):#all the n-1 start points for the subs ending at n
+        for end in range(1, len(nums)):     
+            
+            for start in range(0, end ):
 
                 if nums[end]>nums[start]:
                     
                     dp[end] = max(dp[end], 1+ dp[start])
                     
-        
+       # print(dp)
         return max(dp)
         
