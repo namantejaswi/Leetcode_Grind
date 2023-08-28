dfebbc231ee188bfc8233cf1df67a6498decba20
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-08-28 16:33:10 -0700

    Time: 45 ms (34.08%), Space: 16.4 MB (67.60%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,40 @@
+class MyStack:
+
+    def __init__(self):
+        
+        self.q = deque()
+        
+    def push(self, x: int) -> None:
+        
+        self.q.append(x)
+
+    def pop(self) -> int:
+        
+        last = None
+        for i in range(len(self.q)-1):
+        
+            elem = self.q.popleft()
+            self.q.append(elem)
+            
+            #instead of appending to the same q we could create a new coppy 
+            
+        last = self.q.popleft()
+        return last
+        
+        
+
+    def top(self) -> int:
+        l = len(self.q)
+        return self.q[l-1]       #Return the last element in the que
+
+    def empty(self) -> bool:
+        
+        return len(self.q)==0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
