bb585c22fd3b3c46c14039c01ed979a576502b50
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-09-23 18:52:32 -0700

    Time: 39 ms (53.74%), Space: 16.2 MB (63.15%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        
+        
+        arr = []
+        
+        
+        for i in range(rowIndex+1):
+        
+            arr.append([1]*(i+1))
+            
+        
+        for i in range(2,rowIndex+1):
+            
+            for j in range(1,len(arr[i])-1):    #leave first and last
+                
+                arr[i][j]=arr[i-1][j-1]+arr[i-1][j]
+                
+             
+        return arr[-1]
+            
+            
