447359ae7ba1404504e3cbc6adcdb8fffcd8ec41
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-12-04 19:14:44 -0800

    Time: 122 ms (37.38%), Space: 18.3 MB (32.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,36 +2,41 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         
         
+        #We visually see that we can count the amount of water for each point in a linear pass, visually it is intutive but mathematically 
         
-        length=len(height)
+        #a points water contributing ability is limited by the lower height on either side 
         
-        left=[0]*length
-        right=[0]*length
-        
-        mx_l,mx_r=0,0
-        
-        water=0
+        #each index will contribute water of the amount min( max height to the left from current point, max height to the right)-height[i]
         
         
-        for i in range(length):
+        
+        max_to_left = [0]*len(height)
+        max_to_right = [0]*len(height)
+        
+        
+        mx_l=height[0]
+        for i in range(1,len(height)):
+            mx_l=max(height[i-1],mx_l)
+            max_to_left[i] = mx_l
             
-            if height[i]>mx_l:
-                mx_l=height[i]
-                
-            left[i]=mx_l
+            
+        mx_r=height[-1]
+        for i in range(len(height)-2,-1,-1):
 
-            
-        for j in range(length):
-            
-            if height[length-1-j]>mx_r:
-                mx_r=height[length-j-1]
-                
-            right[length-j-1]=mx_r
+            mx_r = max(height[i+1],mx_r)
+            max_to_right[i] = mx_r
         
+        #print(max_to_left,max_to_right)
+         
+        water = 0    
         
-
-        for k in range(length):
+        for i in range(len(height)):
             
-            water+=min(left[k],right[k])-height[k]
+            contribution =  max(0,min(max_to_left[i],max_to_right[i])-height[i])
+            water+=contribution
             
-        return(water)
+        return water
+            
+            
+            
+            
