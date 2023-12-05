8159b3dd207fe1a4049a8213202fa6dc2fdaf7ba
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-12-05 15:44:18 -0800

    Time: 2633 ms (19.22%), Space: 21.6 MB (8.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,37 +1,35 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
+        #Brute Force O(n^3)
         
         
-        res = set()
+        res =  set()
         
-        nums.sort()    
+        nums.sort()
         
-        for i in  range(len(nums)-1):
         
-            lptr = i +1
+        for i in range(len(nums)-1):    # for last 3
+            
+            lptr = i+1
             rptr = len(nums)-1
         
-        
+
             while lptr<rptr:
-                
-                
-                val= nums[i]+nums[lptr]+nums[rptr]
-                
-                
-                if val==0:  
+
+                val = nums[i]+ nums[lptr] + nums[rptr]
+
+                if val ==0: 
                     res.add((nums[i],nums[lptr],nums[rptr]))
-                    lptr+=1
-    
-                elif val>0: rptr-=1
-            
-                elif val<0: lptr+=1
+                    lptr+=1      #see if theres another cobination to the right of i   
                     
-       # print(res) 
-        
-        ans = []
+                elif val<0: lptr+=1
+                elif val>0: rptr-=1
+                
+                
+        #print(res)
+        ans=[]
         for tupple in res:
             ans.append(list(tupple))
             
         return ans
-            
