c8572afe92be061f7466df740427235e4038ee3c
Naman Tejaswi <tejaswinaman1@gmail.com>
2023-12-20 15:07:51 -0500

    Time: 54 ms (97.07%), Space: 17.5 MB (25.45%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -0,0 +1,21 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        
+        min_1 = float('inf')
+        min_2 = float('inf')
+        
+        
+        for p in prices:
+            
+            if p <min_1:
+                
+                min_2 = min_1
+                min_1 = p
+                
+            else:
+                
+                min_2 = min(p,min_2)
+                
+        print(min_1,min_2)
+        if money -min_1-min_2 >=0: return money-min_1-min_2
+        else:   return money
