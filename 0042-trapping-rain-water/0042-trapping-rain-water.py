6d3a29f8777106f6b706e33cb341c2282b50a393
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-01 03:49:03 -0500

    Time: 269 ms (7.90%), Space: 19.3 MB (9.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,41 +2,35 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         
         
-        #We visually see that we can count the amount of water for each point in a linear pass, visually it is intutive but mathematically 
-        
-        #a points water contributing ability is limited by the lower height on either side 
-        
-        #each index will contribute water of the amount min( max height to the left from current point, max height to the right)-height[i]
-        
-        
-        
-        max_to_left = [0]*len(height)
-        max_to_right = [0]*len(height)
+        max_to_the_left = [0]*len(height)
+        max_to_the_right = [0]*len(height)
         
         
         mx_l=height[0]
-        for i in range(1,len(height)):
-            mx_l=max(height[i-1],mx_l)
-            max_to_left[i] = mx_l
-            
-            
         mx_r=height[-1]
-        for i in range(len(height)-2,-1,-1):
-
-            mx_r = max(height[i+1],mx_r)
-            max_to_right[i] = mx_r
         
-        #print(max_to_left,max_to_right)
-         
-        water = 0    
+        
+        for i in range(1,len(height)):
+            
+            mx_l = max(height[i],mx_l)
+            
+            max_to_the_left[i]=mx_l
+            
+        for i in range(len(height)-2,-1,-1):
+        
+            mx_r = max(height[i],mx_r)
+            
+            max_to_the_right[i] =mx_r
+            
+        #print(max_to_the_left,max_to_the_right)    
+        #Now water stored by each index of height h will be
+        # min(mx_to_the_left,mx_to the right)-h
+        
+        total=0
+        
         
         for i in range(len(height)):
             
-            contribution =  max(0,min(max_to_left[i],max_to_right[i])-height[i])
-            water+=contribution
-            
-        return water
-            
-            
-            
-            
+            total+= max(0,min(max_to_the_left[i], max_to_the_right[i])-height[i])
+        
+        return total
