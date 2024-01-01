cd914e1b4fbe6c2735e956fc85da993c0209b117
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-01 05:29:36 -0500

    Time: 1785 ms (5.01%), Space: 44 MB (24.60%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,7 +6,7 @@ class Solution:
 
         l1=len(s1)
         l2=len(s2)
-        def lcs(s1,s2,l1,l2):
+        def lcs(l1,l2):
 
             if l1>=len(s1) or l2>=len(s2):  return 0
 
@@ -14,12 +14,12 @@ class Solution:
 
             if s1[l1]==s2[l2]:
 
-                dp[l1][l2]= 1+lcs(s1,s2,l1+1,l2+1)
+                dp[l1][l2]= 1+lcs(l1+1,l2+1)
                 return dp[l1][l2]
 
             else:
-                dp[l1][l2] = max( lcs(s1,s2,l1+1,l2),lcs(s1,s2,l1,l2+1))
+                dp[l1][l2] = max( lcs(l1+1,l2),lcs(l1,l2+1))
 
                 return dp[l1][l2]
 
-        return lcs(s1,s2,0,0)
+        return lcs(0,0)
