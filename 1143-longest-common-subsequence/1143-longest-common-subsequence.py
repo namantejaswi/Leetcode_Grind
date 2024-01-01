d722ba554f7e476bacd90b41cc770834d127909d
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-01 05:27:44 -0500

    Time: 1748 ms (5.26%), Space: 44.1 MB (24.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,28 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
         
-        dp = {}
         
-          
-            
-            
-        def lcs(l1,l2):
-            
-            
-            if l1 == len(s1)  or l2 == len(s2): return 0 
-            
-            elif (l1,l2) in dp:    return dp[(l1,l2)]
-            
-            elif s1[l1]==s2[l2]:
-                
-                dp[(l1,l2)]= 1+lcs(l1+1,l2+1) 
-                return dp[(l1,l2)]            
-            
-            else:   
-                dp[(l1,l2)]=max(lcs(l1+1,l2),lcs(l1,l2+1))
-                return dp[(l1,l2)]
-        
-        return lcs(0,0)
-      
-            
-        
+        dp = [[ -1 for i in range(len(s2))]for j in range(len(s1))]
+
+        l1=len(s1)
+        l2=len(s2)
+        def lcs(s1,s2,l1,l2):
+
+            if l1>=len(s1) or l2>=len(s2):  return 0
+
+            if dp[l1][l2]!=-1: return dp[l1][l2]
+
+            if s1[l1]==s2[l2]:
+
+                dp[l1][l2]= 1+lcs(s1,s2,l1+1,l2+1)
+                return dp[l1][l2]
+
+            else:
+                dp[l1][l2] = max( lcs(s1,s2,l1+1,l2),lcs(s1,s2,l1,l2+1))
+
+                return dp[l1][l2]
+
+        return lcs(s1,s2,0,0)
