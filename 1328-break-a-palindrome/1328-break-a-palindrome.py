65457fbae17b60ee4e4b819accb6b304bb4f62f2
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-01 01:13:24 -0500

    Time: 76 ms (5.05%), Space: 17.2 MB (11.72%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,21 +1,26 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-
         
         
-        palindrome=list(palindrome)
-    
+        palindrome = list(palindrome)
         if len(palindrome)==1:  return ""
-  
-        for i in range(len(palindrome)//2):
+        
+        
+        else:
             
-                    
-            
-                if palindrome[i]!='a':
-                    palindrome[i]='a'
-                    return (''.join(palindrome))
-            
+            for i in range(len(palindrome)//2):
                 
-        palindrome[-1]='b'
-        return (''.join(palindrome))
-            
+                
+                # we try to insert 'a' at the earliest spot
+                if palindrome[i]!='a':  
+                    palindrome[i]='a'
+                    return "".join(palindrome)
+                    
+        #if we have a string of all a's we insert b at the last position
+        
+        
+        
+        palindrome[-1]="b"
+        
+        
+        return "".join(palindrome)
