26ecb867e8109a51dda23cf6aad9737bed2ce9c2
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-04 17:04:10 -0500

    Time: 27 ms (98.48%), Space: 17.3 MB (20.74%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -0,0 +1,45 @@
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        
+        
+        res=""
+        
+        if numerator%denominator == 0:  
+
+            return str(numerator//denominator)  #to avoid unecessary .0
+        
+        
+        if numerator*denominator <0:  res+="-"
+            
+            
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+        
+        qoutient,remainder = divmod(numerator,denominator)
+        res+= str(qoutient)
+        
+        seen_remainder = {}
+        
+        
+        decimal=""
+        idx=0
+        while remainder>0 and remainder not in seen_remainder:
+            
+            seen_remainder[remainder] = idx
+            idx+=1
+            
+            q,remainder = divmod(remainder*10, abs(denominator))
+            decimal+=str(q)
+            
+        #If we have a repeating deciaml i.e. remainder    
+        if remainder in seen_remainder:     
+            
+            idx=seen_remainder[remainder]
+            return res+  '.' + decimal[:idx] + '(' + decimal[idx:] + ')'
+        
+        else: return res+"."+decimal[:]
+            
+            
+            
+            
+        
