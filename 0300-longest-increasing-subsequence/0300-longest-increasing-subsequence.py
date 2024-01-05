53cab0f3537c78a9bc06e502a178afa5a22556be
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-04 19:33:13 -0500

    Time: 1802 ms (66.26%), Space: 17.7 MB (20.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,18 +1,16 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
+        #dp[i] represents the length of longest subseq ending at i
         
-        dp = [ 1 for i in range(len(nums))] 
+        dp = [ 1 for i in range(len(nums))]
+        
+        for end in range(1,len(nums)):
+            
+            for start in range(0,end):
                 
-            
-        for end in range(1, len(nums)):     
-            
-            for start in range(0, end ):
-
                 if nums[end]>nums[start]:
                     
-                    dp[end] = max(dp[end], 1+ dp[start])
+                    dp[end]=max(1+dp[start],dp[end])
                     
-       # print(dp)
         return max(dp)
-        
