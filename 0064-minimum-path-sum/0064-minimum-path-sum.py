a91eb8af5b4b86aa911e35906d04f8e0cd8359a0
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-05 20:08:10 -0500

    Time: 77 ms (98.07%), Space: 20.2 MB (15.68%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -15,19 +15,17 @@ class Solution:
             for j in range(w):
                 
 
-                if i==0 and j==0:   
-                    print(grid[0][0])
-                    dp[i][j]=grid[0][0]
+                if i==0 and j==0:   pass
 
-                elif j==0 and i!=0:    dp[i][j]= dp[i-1][j]+grid[i][j]
+                elif j==0 and i!=0:    grid[i][j]+= grid[i-1][j]
                     
-                elif i==0 and j!=0: dp[i][j] = dp[i][j-1]+grid[i][j]
-                    
+                elif i==0 and j!=0: grid[i][j] +=grid[i][j-1]
+                
                 else:   
-                    dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1])
+                    grid[i][j]+=min(grid[i-1][j],grid[i][j-1])
                      
                 
-        return dp[-1][-1]
+        return grid[-1][-1]
                     
                     
                 
