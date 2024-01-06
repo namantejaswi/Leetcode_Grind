e21f1abc760e833f4bbe9f704c052c77c3f4c37b
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-06 13:19:41 -0500

    Time: 120 ms (45.65%), Space: 19.2 MB (20.00%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,36 +1,31 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
+        #we try to calculate the water trapped by each index
+        #water trapped by each index is limited by the largest element to the left and right , min(max_to_the_left,max_to the_right)-h
         
-        max_to_the_left = [0]*len(height)
-        max_to_the_right = [0]*len(height)
+        max_to_left = [0 for i in range(len(height))]
+        max_to_right = [0 for j in range(len(height))]
         
-        
-        mx_l=height[0]
-        mx_r=height[-1]
+        mx_l = height[0]
+        mx_r = height[-1]
         
         
         for i in range(1,len(height)):
-            
-            mx_l = max(height[i],mx_l)
-            
-            max_to_the_left[i]=mx_l
-            
-        for i in range(len(height)-2,-1,-1):
         
-            mx_r = max(height[i],mx_r)
+            max_to_left[i] = mx_l
+            mx_l = max(mx_l,height[i])
             
-            max_to_the_right[i] =mx_r
             
-        #print(max_to_the_left,max_to_the_right)    
-        #Now water stored by each index of height h will be
-        # min(mx_to_the_left,mx_to the right)-h
+        for j in range(len(height)-2,-1,-1):
+            max_to_right[j] = mx_r
+            mx_r = max(mx_r, height[j])
         
-        total=0
+        total = 0
         
-        
-        for i in range(len(height)):
+        for h in range(len(height)):
             
-            total+= max(0,min(max_to_the_left[i], max_to_the_right[i])-height[i])
-        
+            total += max(min(max_to_left[h],max_to_right[h])-height[h], 0)
+            
         return total
+            
