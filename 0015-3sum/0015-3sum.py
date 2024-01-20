84cea9cba2c52cedb46f88e228f2aa0185ffcf2c
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-01-20 17:26:10 -0500

    Time: 1084 ms (49.32%), Space: 22.1 MB (8.85%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,51 +2,47 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
         
-        res = set()
-        
+        res = []
+        triplet = set()
         nums.sort()
-
-        def two_sum(idx):
         
+        #we find 2 numbers such that nums[idx]+nums[n1]+nums[n2]=target
+        
+        def two_sum(idx):
             
-            lptr = idx + 1
-            rptr = len(nums)-1
-
+            lptr=idx+1
+            rptr=len(nums)-1
+            
             while lptr<rptr:
-
-                val = nums[lptr]+nums[rptr]+nums[idx]
-                if val==0:  
-                    res.add((nums[idx],nums[lptr],nums[rptr]))
+                val= nums[idx] + nums[lptr] + nums[rptr]
+                    
+                if val==0: 
+                    
+                    triplet.add((nums[idx],nums[lptr],nums[rptr]))
                     lptr+=1
                     rptr-=1
+                    
+                    
                     while lptr<rptr and nums[lptr]==nums[lptr-1]:   lptr+=1
                     
                     
-                elif val<0: lptr+=1
-
+                    
                 elif val>0: rptr-=1
+                    
+                elif val<0: lptr+=1
+                    
+                    
                 
-
-        
         for i in range(len(nums)):
-            
-        #if curr is +ve the remainins numbeers to right are also+ve so cant sum to 0
-            
-            if nums[i]>0:   break 
-                
-            else:  two_sum(i)
-                
-            
-        ans = []
-        for tupple in res:
-            ans.append(list(tupple))
-            
-        return ans
-                
-                
 
-                
-                
-                
+            if nums[i]>0:   break
+
+            else:   two_sum(i)
+
+
+        for tup in triplet:
+            res.append(list(tup))
+
+        return res
+
         
-            
