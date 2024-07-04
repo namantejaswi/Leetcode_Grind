16cc84ae8ece4340d7fc24e2d792941fdb39c0ae
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-07-04 04:59:35 -0400

    Time: 74 ms (43.42%), Space: 16.9 MB (92.11%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,15 +1,43 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-       
-        max_prod = min_prod = res = nums[0]
-         
-        for i in range(1, len(nums)):
-            
-            temp = max_prod
-            
-            max_prod = max(nums[i], max_prod * nums[i], min_prod * nums[i])
-            min_prod = min(nums[i], temp * nums[i], min_prod * nums[i])
-            
-            res = max(res, max_prod)
         
-        return res
+        
+        #represent max,min prod ending at curr index
+        max_prod = 1
+        min_prod = 1
+        mx=0
+        flag = 0    #to see if we only have a single negative i.e ensure at  least 1+ve or 2 consequetive -ve at the very least
+        
+        if len(nums)==1: return nums[0]
+        
+        for i in range(len(nums)):
+            
+            if nums[i] == 0: 
+                
+                max_prod , min_prod = 1,1
+            
+            elif nums[i] >0:
+                flag=1
+                max_prod = max(max_prod*nums[i],nums[i])
+                
+                min_prod = min_prod*nums[i]
+            
+                mx = max(mx,max_prod)
+            
+            elif nums[i]<0:
+                
+                mi = min_prod #temp var
+                
+                min_prod = min(max_prod*nums[i],1)
+                
+                max_prod = max(mi*nums[i],1)
+                
+                mx = max(mx,max_prod)
+                
+                if mi*nums[i] >0: flag=1
+        print(flag)          
+        if flag==0 : return  0
+        if flag==0: return max(nums)
+        return mx
+                   
+        
