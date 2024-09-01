d791895c10d8507734a7d50453439ebef2ba3c81
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-08-31 21:20:44 -0400

    Time: 49 ms (38.25%), Space: 16.7 MB (62.87%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -2,24 +2,32 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
         
+        #Peak element is the left most element which is greater than next next
+        if len(nums)==1:    return 0
         
-        #Peak exists means the array before peak is increasing and array after peak is decreasing 
-        
-        # 
-        
-        l = 0
+        l = 0 
         r = len(nums)-1
-        res = None
+        ans = None
         
         
-        while l<r:
+        if nums[l]>nums[l+1]:
+            return l
+        
+        elif nums[r]>nums[r-1]: return r
+        
+        while l<=r:
+            
+            
             mid = (l+r)//2
             
-            if nums[mid]>nums[mid+1]:   #mid can be peak 
-                r = mid
+            
+            if nums[mid]>nums[mid+1]:
+                pos = mid       #mid is valid we store it and greedily look for nect best in left
+                r = mid-1
                 
             else:
-                l = mid+1   #mmid canot be peak  mid+1 can be
+                l=mid+1
                 
                 
-        return l
+        return pos
+                
