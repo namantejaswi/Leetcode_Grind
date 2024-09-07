e16373e6d880e057a33f79ce840700a20fc1d144
Naman Tejaswi <tejaswinaman1@gmail.com>
2024-09-07 09:41:29 -0400

    Time: 259 ms (24.09%), Space: 43.5 MB (15.22%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.py
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -7,15 +7,34 @@
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
         
+        
+        
+        def check_odd_increasing(nums):
+            if nums[0]%2==0:    return False
+            for i in range(1,len(nums)):
+
+                if nums[i]<=nums[i-1] or nums[i]%2==0:
+                    return False
+
+
+        def check_even_decreasing(nums):
+            if nums[0]%2!=0:    return False
+            for i in range(1,len(nums)):
+
+                if nums[i]>=nums[i-1] or nums[i]%2==1:
+                    return False
+
         if root.val%2==0:   return False
     
         
         q=deque([root])
         
         res = []
+        idx=0
         while q:
             
             lvl = []
+            
             
             for i in range(len(q)):
                 
@@ -33,24 +52,14 @@ class Solution:
         #print(res)
         #we can reduce axuilary space to O(width) or O(2*height) or O(logn) by checking each level as we generate it it and it would also help us return early
         
-        
-        idx=0
-        for nums in res:
-              
-                if idx%2==0:
-                    if nums[0]%2==0:    return False
-                    for i in range(1,len(nums)):
-                        
-                        if nums[i]<=nums[i-1] or nums[i]%2==0:
-                            return False
-                        
-                elif idx%2==1:
-                    if nums[0]%2!=0:    return False
-                    for i in range(1,len(nums)):
-                        
-                        if nums[i]>=nums[i-1] or nums[i]%2==1:
-                            return False
-                idx+=1         
-        
-        return True            
+            if idx%2==0:    
+                flag = check_odd_increasing(lvl)
+                if flag == False:   return False
+            else:  
+                flag=check_even_decreasing(lvl)
+                if flag == False:   return False
+
+            idx+=1
                     
+
+        return True
